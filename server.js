7aac7fe751dc423d77ecaf48e93b6e68e5ba15e5
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-03-12 15:07:11 +0200

    Removed .env file, modified server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require('http');
+// Server dependecies
 const express = require('express');
 
-const app = express()
-const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 app.get('/', (request, resource) => {
     resource.send('University Prospects Server')
 })
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`University prospects listening on port ${PORT}`)
 })
