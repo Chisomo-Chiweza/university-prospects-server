4923769530886a206ab1e8b2ff5edce60832fa26
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-03-23 15:44:59 +0200

    Bug with server
    Page not loading. Removed all faulty endpoints to debug problems

--- server.js
+++ server.js
@@ -11,22 +11,6 @@ app.get('/', async (request, resource) => {
     resource.send('University Prospects')
 })
 
-// Endpoints
-app.get('/universities', async (request, resource) => {
-    const universities = await prisma.university()
-    resource.send(universities)
-})
-
-app.get('/faculties', async (request, resource) => {
-    const faculties = await prisma.faculty()
-    resource.send(universities)
-})
-
-app.get('/programs', async (request, resource) => {
-    const programs = await prisma.program()
-    resource.send(universities)
-})
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`University prospects listening on port ${PORT}`)
