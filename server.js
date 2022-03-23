2718eebc93cbd445a21816709ac1a3bcc2fd672b
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-03-23 15:41:50 +0200

    Added test endpoints

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 // Server dependecies
-const express = require('express');
+import express from "express";
+import { PrismaClient } from '@prisma/client';
 
+// Define our express app and prisma orm
+const prisma = new PrismaClient()
 const app = express();
 
+// Home
+app.get('/', async (request, resource) => {
+    resource.send('University Prospects')
+})
 
-app.get('/', (request, resource) => {
-    resource.send('University Prospects Server')
+// Endpoints
+app.get('/universities', async (request, resource) => {
+    const universities = await prisma.university()
+    resource.send(universities)
+})
+
+app.get('/faculties', async (request, resource) => {
+    const faculties = await prisma.faculty()
+    resource.send(universities)
+})
+
+app.get('/programs', async (request, resource) => {
+    const programs = await prisma.program()
+    resource.send(universities)
 })
 
 const PORT = process.env.PORT || 3001;
