0ded622ba96151a20304ed06646e6dd4ae3186ee
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-04-01 20:19:57 +0200

    Made some changes

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-// Server dependecies
-const express = require('express')
+// Import dependencies
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-// Define our express app and prisma orm
-const app = express();
+
+// Define the Express app
+var app = express();
+
+// Use helmet to enhance API security
+app.use(helmet())
+
+// Use body-parser to parse JSON bodies into JS objects
+app.use(bodyparser.json());
+
+// Use morgan to log HTTP requests
+app.use(morgan("combined"));
+
+// Enable CORS for university prospects requests
+app.use(cors({
+    origin: ["https://www.universityprospects.co/", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+
+// Prisma database
+
+
+
+
+// Endpoints
 
 // Home
-app.get('/', async (request, resource) => {
-    resource.send('University Prospects')
+app.get("/", cors(), (request, resource) => {
+    resource.send("University Prospects API");
 })
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`University prospects listening on port ${PORT}`)
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Server is running on port 3001");
 })
+
+
+
