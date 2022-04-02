81c428d5c1abdad7c92c63b8ae7eab76e5737e92
Chisomo Chiweza <chisomochiwezapersonal@gmail>
2022-04-02 21:15:07 +0200

    Modifying Server hosted on Heroku
    - Managed to add 2 rows using prisma studio to database at Heroku
    - Server stopped unexpectedly so modifying it to troubleshoot issue

--- server.js
+++ server.js
@@ -35,41 +35,10 @@ app.get("/", cors(), (request, resource) => {
     resource.send("University Prospects API");
 })
 
-// Add university
-app.post('/university', async (request, resource) => {
-    const university = await prisma.universities.create({ data: request.body });
-    request.json(university);
-})
-
-// Add faculty
-app.post('/faculty', async (request, resource) => {
-    const faculty = await prisma.faculties.create({ data: request.body });
-    request.json(faculty);
-})
-
-// Add programme
-app.post('/programme', async (request, resource) => {
-    const programme = await prisma.programmes.create({ data: request.body });
-    request.json(programme);
-})
-
-
 // Get all universities
 app.get('/universities', async (request, resource) => {
     const universities = await prisma.universities.findMany();
     request.json(universities);
-})
-
-// Get all faculties
-app.get('/faculties', async (request, resource) => {
-    const faculties = await prisma.faculties.findMany();
-    request.json(faculties);
-})
-
-// Get all programmes
-app.get('/programmes', async (request, resource) => {
-    const programmes = await prisma.programmes.findMany();
-    request.json(programmes);
 })
 
 app.listen(process.env.PORT || 3001, () => {
